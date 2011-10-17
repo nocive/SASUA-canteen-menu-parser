fcb7d96bffae5402ed543051e47db8f34e4fbf7d
José Pedro Saraiva <nocive@gmail.com>
2011-10-17 11:41:08 +0100

    pushing existing code

---
+++ example.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once( 'sasua_canteens.php' );
+
+//
+// web applications
+//
+$_GET['z'] = 'santiago';
+$_GET['t'] = 'day';
+$_GET['f'] = 'json';
+$sas = new SASUA_Canteens_Web();
+
+
+echo "\n\n\n";
+
+
+//
+// console applications
+//
+$sas = new SASUA_Canteens();
+echo $sas->get( 'santiago', 'day', 'xml' );
+
+?>
